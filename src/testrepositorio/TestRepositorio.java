4b44ef47f9bf2f2604922bc3f2efacedf14d7744
Marcosd3laULP <usuario4ltnum1@gmail.com>
2024-08-19 16:57:33 -0300

    Initial commit

---
+++ src/testrepositorio/TestRepositorio.java
@@ -0,0 +1,12 @@
+
+package testrepositorio;
+
+
+public class TestRepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hola a mi primer repositorio :D");
+        
+    }
+    
+}
